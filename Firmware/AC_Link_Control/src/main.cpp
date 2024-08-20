4dc9054688419f9b597a1440d44767f8cea45487
lilindian14 <jaime@astutesmartlocks.com>
2024-08-20 14:51:40 +1200

    Update main source to remove use of AtmelAVR framwork

--- Firmware/AC_Link_Control/src/main.cpp
+++ Firmware/AC_Link_Control/src/main.cpp
@@ -1,26 +1,37 @@
 #include <Arduino.h>
-#include <9BitUART.hpp>
+#include <AudisonACLink.hpp>
 
 #define AUDISON_BIT10_RS485_ADDRESS 0x00
 #define AUDISON_DRC_RS485_ADDRESS 0x80
 
-UART_9BIT Audison_AC_Link_Bus;
+#define RS485_TX_PIN 15
+#define RS485_RX_PIN 6
+#define RS485_TX_EN_PIN 2
+
+AudisonACLink ac_link;
 
 void setup(void)
 {
-  Audison_AC_Link_Bus.init_uart(38400);
+  Serial.begin(115200);
+  RS485_Config_t rs485_config;
+  rs485_config.rs485_tx_pin = RS485_TX_PIN;
+  rs485_config.rs485_rx_pin = RS485_RX_PIN;
+  rs485_config.rs485_tx_en_pin = RS485_TX_EN_PIN;
+  ac_link.init_ac_link_bus(&rs485_config);
 }
 
 void loop(void)
 {
   /* Trial trnasmit function. We can send volume adjust packets for now */
-  for (uint8_t i = 0; i < 0x78; i++)
+  for (uint8_t i = 0; i <= 0x78; i++)
   {
     uint8_t volume_adjust_packet[2] = {0x0A, i};
-    Audison_AC_Link_Bus.write_to_audison_bus(AUDISON_BIT10_RS485_ADDRESS,
-                                             AUDISON_DRC_RS485_ADDRESS,
-                                             (uint8_t *)volume_adjust_packet,
-                                             sizeof(volume_adjust_packet));
+    ac_link.write_to_audison_bus(AUDISON_BIT10_RS485_ADDRESS,
+                                 AUDISON_DRC_RS485_ADDRESS,
+                                 (uint8_t *)volume_adjust_packet,
+                                 sizeof(volume_adjust_packet));
+    Serial.print("Volume: ");
+    Serial.println(i, HEX);
     delay(1000);
   }
 }
