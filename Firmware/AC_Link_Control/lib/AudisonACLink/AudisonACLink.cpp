589a1ffa5b0ce9642d5662d3a4f81d65b4ee626a
lilindian14 <jaime@astutesmartlocks.com>
2024-08-21 08:42:10 +1200

    Update - Class functions
    Updated class functions to discretise the public functions into what should actually be public

--- Firmware/AC_Link_Control/lib/AudisonACLink/AudisonACLink.cpp
+++ Firmware/AC_Link_Control/lib/AudisonACLink/AudisonACLink.cpp
@@ -1,11 +1,10 @@
 /**
- * Software specifically for the Audison Bit 10 DRC using the AC Link Bus (RS485)
+ * Software specifically for integrating into the Audison AC Link Bus (RS485)
  *
  * Audison AC Link bus uses 9-bit serial data on the RS485 bus. The 9th bit of the data
- * indicates whether the data is an address (1) or data byte (0).
+ * indicates whether the data is an address (1) or data byte (0)
  *
- * Microcontroller used: ESP32
- * Module: Wrover-IE
+ * MCU used: ESP32 WROVER-IE Module (ESP32-D0WD-V3)
  *
  * Author: Jaime Sequeira
  */
@@ -16,6 +15,8 @@
 
 #define HEADER_SIZE_BYTES 4
 #define CHECKSUM_SIZE_BYTES 1
+#define AUDISON_DSP_RS485_ADDRESS 0x00
+#define AUDISON_DRC_RS485_ADDRESS 0x80
 
 EspSoftwareSerial::UART rs485_serial_port;
 
@@ -29,23 +30,32 @@ void AudisonACLink::init_ac_link_bus(RS485_Config_t *rs485_config)
     digitalWrite(this->tx_en_pin, LOW); // RX EN pin is always low, we control flow with the TX pin going high
 }
 
+void AudisonACLink::set_volume(uint8_t volume)
+{
+    if (volume <= 0x78)
+    {
+        uint8_t volume_adjust_packet[2] = {VOLUME_ADJUST, volume};
+        this->write_to_audison_bus(AUDISON_DSP_RS485_ADDRESS,
+                                   AUDISON_DRC_RS485_ADDRESS,
+                                   (uint8_t *)volume_adjust_packet,
+                                   sizeof(volume_adjust_packet));
+    }
+}
+
 void AudisonACLink::write_to_audison_bus(uint8_t receiver_address, uint8_t transmitter_address, uint8_t *data, uint8_t data_length)
 {
-    uint32_t byte_sum = 0; // We use the sum of bytes for the checksum calculation
     uint8_t message_length = HEADER_SIZE_BYTES + data_length + CHECKSUM_SIZE_BYTES;
     uint8_t message_buffer[message_length];
     message_buffer[0] = receiver_address;
     message_buffer[1] = transmitter_address;
     message_buffer[2] = 0x00;
     message_buffer[3] = message_length;
-    byte_sum += (receiver_address + transmitter_address + message_length); // Sum the bytes so far
     for (uint8_t i = 0; i < data_length; i++)
     {
         message_buffer[i + 4] = data[i];
-        byte_sum += data[i];
     }
     /* Now we append the checksum - CheckSum8 Modulo 256 */
-    uint8_t checksum = byte_sum % 256;
+    uint8_t checksum = this->calculate_checksum(message_buffer, sizeof(message_buffer) - 1); // Last byte is the checksum
     message_buffer[message_length - 1] = checksum;
 
     digitalWrite(this->tx_en_pin, HIGH); // TX output enable
@@ -63,4 +73,15 @@ void AudisonACLink::write_to_audison_bus(uint8_t receiver_address, uint8_t trans
     }
 
     digitalWrite(this->tx_en_pin, LOW); // TX output disable
+}
+
+uint8_t AudisonACLink::calculate_checksum(uint8_t *data_buffer, uint8_t data_length_bytes)
+{
+    uint32_t byte_sum = 0;
+    for (uint8_t i = 0; i < data_length_bytes; i++)
+    {
+        byte_sum += data_buffer[i];
+    }
+    uint8_t checksum = byte_sum % 256;
+    return checksum;
 }
--- Firmware/AC_Link_Control/lib/AudisonACLink/AudisonACLink.hpp
+++ Firmware/AC_Link_Control/lib/AudisonACLink/AudisonACLink.hpp
@@ -1,15 +1,10 @@
 /**
- * 9-bit UART
- * Software specifically for the Audison Bit 10 DRC using the AC Link Bus (RS485)
+ * Software specifically for integrating into the Audison AC Link Bus (RS485)
  *
  * Audison AC Link bus uses 9-bit serial data on the RS485 bus. The 9th bit of the data
- * indicates whether the data is an address (1) or data byte (0).
+ * indicates whether the data is an address (1) or data byte (0)
  *
- * Microcontroller used: ATMEGA328P - Arduino Uno
- *
- * Connections for ATMEGA328P with RS485 transceiver:
- *      MCU TX - Pin 1 -> RS485_TX
- *      MCU_RX - Pin 0 -> RS485_RX
+ * MCU used: ESP32 WROVER-IE Module (ESP32-D0WD-V3)
  *
  * Author: Jaime Sequeira
  */
@@ -26,20 +21,46 @@ typedef struct
     unsigned long rs485_baudrate = 38400;
 } RS485_Config_t;
 
+enum AC_Link_Command
+{
+    VOLUME_ADJUST = 0x0A,
+    BALANCE_ADJUST = 0x0B,
+    FADER_ADJUST = 0x0C,
+    SUB_VOLUME_ADJUST = 0x0D,
+};
+
 class AudisonACLink
 {
-public:
+public: // Public functions
+    /**
+     * @param rs485_config Config struct pointer
+     */
     void init_ac_link_bus(RS485_Config_t *rs485_config);
 
     /**
-     * @param receiver_address
+     * @param volume Value between min and max volume (0x00 to 0x78)
+     */
+    void set_volume(uint8_t volume);
+
+public:  // Public Variables
+private: // Private functions
+    /**
+     * @param receiver_address Used for transmission. Parity bit will be marked to indicate address
      * @param transmitter_address
      * @param data Buffer for data to be sent in transaction
      * @param data_length Length of the data buffer
      */
     void write_to_audison_bus(uint8_t receiver_address, uint8_t transmitter_address, uint8_t *data, uint8_t data_length);
 
-private:
+    /**
+     * @param data_buffer   Buffer of data to be checksummed
+     * @param data_length_bytes
+     *
+     * @returns Checksum byte (8-bit module 256)
+     */
+    uint8_t calculate_checksum(uint8_t *data_buffer, uint8_t data_length_bytes);
+
+private: // Private Variables
     int tx_pin;
     int rx_pin;
     int tx_en_pin;
