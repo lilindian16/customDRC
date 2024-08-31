484199f28ff56f8633802881e583919afc0e6933
lilindian14 <jaime@astutesmartlocks.com>
2024-09-01 10:36:34 +1200

    Updated main source for latest AC Link lib updates

--- Firmware/AC_Link_Control/src/main.cpp
+++ Firmware/AC_Link_Control/src/main.cpp
@@ -2,10 +2,15 @@
 #include <AudisonACLink.hpp>
 
 #define RS485_TX_PIN 15
-#define RS485_RX_PIN 6
+#define RS485_RX_PIN 5
 #define RS485_TX_EN_PIN 2
 
+// #define TRIAL_TRANSMIT
+#define TRIAL_RECEIVE
+
 AudisonACLink ac_link;
+
+uint8_t rx_buffer[255];
 
 void setup(void)
 {
@@ -15,10 +20,13 @@ void setup(void)
   rs485_config.rs485_rx_pin = RS485_RX_PIN;
   rs485_config.rs485_tx_en_pin = RS485_TX_EN_PIN;
   ac_link.init_ac_link_bus(&rs485_config);
+
+  memset(rx_buffer, 0, sizeof(rx_buffer)); // Clear the rx buffer
 }
 
 void loop(void)
 {
+#ifdef TRIAL_TRANSMIT
   /* Trial transmit function. We can send volume adjust packets for now */
   for (uint8_t i = 0; i <= 0x78; i++)
   {
@@ -27,4 +35,22 @@ void loop(void)
     Serial.println(i, HEX);
     delay(250);
   }
+#endif
+#ifdef TRIAL_RECEIVE
+  uint8_t bytes_read = ac_link.read_rx_message(rx_buffer, sizeof(rx_buffer));
+  if (bytes_read > 0)
+  {
+    if (rx_buffer[1] == 0x80) // Filter DRC messages
+    {
+      for (uint8_t i = 0; i < bytes_read; i++)
+      {
+        Serial.print(rx_buffer[i], HEX);
+        Serial.print(" ");
+      }
+      Serial.println();
+    }
+    memset(rx_buffer, 0, sizeof(rx_buffer)); // Clear the buffer
+  }
+  delay(50);
+#endif
 }
