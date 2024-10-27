1cd444a6913e7e9ac172c329654183f22b6038b2
lilindian14 <jaime@astutesmartlocks.com>
2024-10-27 14:37:39 +1300

    Added funcs to enable and disable encoders

--- Firmware/AC_Link_Control/lib/CustomDRC/DRC_Encoder.cpp
+++ Firmware/AC_Link_Control/lib/CustomDRC/DRC_Encoder.cpp
@@ -1,37 +1,50 @@
 #include "DRC_Encoder.hpp"
-#include <Arduino.h>
-#include <ESP32Encoder.h>
+
 #include "Audison_AC_Link_Bus.hpp"
 #include "CDRCWebServer.hpp"
+#include "Custom_DRC.hpp"
 
-#define ENCODER_1_A 35
-#define ENCODER_1_B 32
-#define ENCODER_1_SW 33
-
-#define ENCODER_2_A GPIO_NUM_36
-#define ENCODER_2_B GPIO_NUM_39
-#define ENCODER_2_SW GPIO_NUM_34
+#include <Arduino.h>
+#include <ESP32Encoder.h>
 
 constexpr uint8_t ENCODER_1_ID = 0x01;
 constexpr uint8_t ENCODER_2_ID = 0x02;
+
+#define BUTTON_HELD_TIME_SECONDS 1
+#define ENCODER_TASK_PERIOD_MS 50
+#define BUTTON_HELD_TICKS_REQUIRED (BUTTON_HELD_TIME_SECONDS * 1000 / ENCODER_TASK_PERIOD_MS)
 
 ESP32Encoder encoder_1(true, enc_cb);
 ESP32Encoder encoder_2(true, enc_cb);
 TaskHandle_t encoder_task_handle;
 SemaphoreHandle_t encoder_1_semaphore_handle, encoder_2_semaphore_handle;
 
+struct DSP_Settings *dsp_settings_encoders;
+
+volatile bool encoders_enabled = true;
+
 void encoder_task(void *pvParameters); // Forward declaration
 
 static IRAM_ATTR void enc_cb(void *arg)
 {
-    ESP32Encoder *enc = (ESP32Encoder *)arg;
     BaseType_t xYieldRequired;
-    uint8_t encoder_id = enc->get_encoder_id();
     BaseType_t xHigherPriorityTaskWoken = pdFALSE;
-    if (encoder_id == ENCODER_1_ID)
+
+    ESP32Encoder *enc = (ESP32Encoder *)arg;
+    uint8_t encoder_id = enc->get_encoder_id();
+
+    if (encoders_enabled)
     {
-        xSemaphoreGiveFromISR(encoder_1_semaphore_handle, &xHigherPriorityTaskWoken);
+        if (encoder_id == ENCODER_1_ID)
+        {
+            xSemaphoreGiveFromISR(encoder_1_semaphore_handle, &xHigherPriorityTaskWoken);
+        }
+        else if (encoder_id == ENCODER_2_ID)
+        {
+            xSemaphoreGiveFromISR(encoder_2_semaphore_handle, &xHigherPriorityTaskWoken);
+        }
     }
+
     // Resume the suspended task.
     xYieldRequired = xTaskResumeFromISR(encoder_task_handle);
     // We should switch context so the ISR returns to a different task.
@@ -43,57 +56,114 @@ static IRAM_ATTR void enc_cb(void *arg)
 
 void encoder_task(void *pvParameters)
 {
+    uint8_t button_held_ticks = 0;
+    bool button_held_task_sent = false;
     while (1)
     {
-        if (xSemaphoreTake(encoder_1_semaphore_handle, TickType_t(10)))
+        if (encoders_enabled)
         {
-            int64_t encoder_1_count = encoder_1.getCount(); // Default to using encoder_1 for volume
-            if (encoder_1_count > MAX_VOLUME_VALUE)
+            if (xSemaphoreTake(encoder_1_semaphore_handle, TickType_t(10)))
             {
-                encoder_1.setCount(MAX_VOLUME_VALUE);
-                encoder_1_count = MAX_VOLUME_VALUE;
+                int64_t encoder_1_count = encoder_1.getCount(); // Default to using encoder_1 for volume
+                if (encoder_1_count > MAX_VOLUME_VALUE)
+                {
+                    encoder_1.setCount(MAX_VOLUME_VALUE);
+                    encoder_1_count = MAX_VOLUME_VALUE;
+                }
+                else if (encoder_1_count < MIN_VOLUME_VALUE)
+                {
+                    encoder_1.setCount(MIN_VOLUME_VALUE);
+                    encoder_1_count = MIN_VOLUME_VALUE;
+                }
+                Audison_AC_Link.set_volume(encoder_1_count);
+                update_web_server_parameter(DSP_SETTING_INDEX_MASTER_VOLUME, encoder_1_count);
+                dsp_settings_encoders->master_volume = encoder_1_count;
             }
-            else if (encoder_1_count < MIN_VOLUME_VALUE)
+            if (xSemaphoreTake(encoder_2_semaphore_handle, TickType_t(10)))
             {
-                encoder_1.setCount(MIN_VOLUME_VALUE);
-                encoder_1_count = MIN_VOLUME_VALUE;
+                int64_t encoder_2_count = encoder_2.getCount();
+                if (encoder_2_count > MAX_SUB_VOLUME_VALUE)
+                {
+                    encoder_2.setCount(MAX_SUB_VOLUME_VALUE);
+                    encoder_2_count = MAX_SUB_VOLUME_VALUE;
+                }
+                else if (encoder_2_count < MIN_SUB_VOLUME_VALUE)
+                {
+                    encoder_2.setCount(MIN_SUB_VOLUME_VALUE);
+                    encoder_2_count = MIN_SUB_VOLUME_VALUE;
+                }
+                Audison_AC_Link.set_sub_volume(encoder_2_count);
+                update_web_server_parameter(DSP_SETTING_INDEX_SUB_VOLUME, encoder_2_count);
+                dsp_settings_encoders->sub_volume = encoder_2_count;
             }
-            Audison_AC_Link.set_volume(encoder_1_count);
-            update_web_server_master_volume_value(encoder_1_count);
-            Serial.println("Encoder_1 count = " + String(encoder_1_count));
+            bool current_encoder_1_sw_state = digitalRead(ENCODER_1_SW);
+            if (current_encoder_1_sw_state == LOW)
+            {
+                // Button has been pressed
+                button_held_ticks++;
+                if (button_held_ticks >= BUTTON_HELD_TICKS_REQUIRED)
+                {
+                    button_held_ticks = BUTTON_HELD_TICKS_REQUIRED;
+                    if (!button_held_task_sent)
+                    {
+                        on_button_held();
+                    }
+                }
+            }
+            else if (current_encoder_1_sw_state == HIGH)
+            {
+                // Button released
+                if (button_held_ticks >= BUTTON_HELD_TICKS_REQUIRED)
+                {
+                    // Call method only if button has been held long enough
+                    on_button_released(true);
+                    button_held_task_sent = false; // We can reset the flag now
+                }
+                else
+                {
+                    button_held_ticks = 0;
+                }
+            }
         }
-        if (xSemaphoreTake(encoder_2_semaphore_handle, TickType_t(10)))
-        {
-            int64_t encoder_2_count = encoder_2.getCount();
-            if (encoder_2_count > MAX_SUB_VOLUME_VALUE)
-            {
-                encoder_2.setCount(MAX_SUB_VOLUME_VALUE);
-                encoder_2_count = MAX_SUB_VOLUME_VALUE;
-            }
-            else if (encoder_2_count < MIN_SUB_VOLUME_VALUE)
-            {
-                encoder_2.setCount(MIN_SUB_VOLUME_VALUE);
-                encoder_2_count = MIN_SUB_VOLUME_VALUE;
-            }
-            Audison_AC_Link.set_sub_volume(encoder_2_count);
-            update_web_server_sub_volume_value(encoder_2_count);
-            Serial.println("Encoder_2 count = " + String(encoder_2_count));
-        }
-        vTaskSuspend(NULL);
+        vTaskDelay(pdMS_TO_TICKS(ENCODER_TASK_PERIOD_MS));
     }
 }
 
-void init_drc_encoders(void)
+void init_drc_encoders(struct DSP_Settings *settings)
 {
+    dsp_settings_encoders = settings;
     encoder_1_semaphore_handle = xSemaphoreCreateBinary();
     encoder_2_semaphore_handle = xSemaphoreCreateBinary();
     encoder_1.attachSingleEdge(ENCODER_1_A, ENCODER_1_B);
-    encoder_1.clearCount(); // set starting count value after attaching
+    encoder_1.setCount(dsp_settings_encoders->master_volume);
     encoder_1.setFilter(1023);
     encoder_1.set_encoder_id(ENCODER_1_ID);
     encoder_2.attachSingleEdge(ENCODER_2_A, ENCODER_2_B);
-    encoder_2.clearCount();
+    encoder_2.setCount(dsp_settings_encoders->sub_volume);
     encoder_2.setFilter(1023);
     encoder_2.set_encoder_id(ENCODER_2_ID);
+
+    pinMode(ENCODER_1_SW, INPUT);
+    pinMode(ENCODER_2_SW, INPUT);
+
     xTaskCreatePinnedToCore(encoder_task, "ENCODER", 8000, NULL, tskIDLE_PRIORITY + 1, &encoder_task_handle, 1);
+}
+
+void disable_encoders(void)
+{
+    encoders_enabled = false;
+}
+
+void enable_encoders(void)
+{
+    if (!dsp_settings_encoders->usb_connected)
+    {
+        // Encoders cannot run while USB is plugged in and configuring DSP
+        encoders_enabled = true;
+    }
+}
+
+bool are_encoders_enabled(void)
+{
+    return encoders_enabled;
 }
--- Firmware/AC_Link_Control/lib/CustomDRC/DRC_Encoder.hpp
+++ Firmware/AC_Link_Control/lib/CustomDRC/DRC_Encoder.hpp
@@ -1,5 +1,14 @@
 #pragma once
 #include <Arduino.h>
 
-void init_drc_encoders(void);
+
+void init_drc_encoders(struct DSP_Settings *dsp_settings);
 static IRAM_ATTR void enc_cb(void *arg);
+
+void disable_encoders(void);
+void enable_encoders(void);
+bool are_encoders_enabled(void);
+
+/* Declare these functions - they must be implemented in main */
+extern void on_button_held();
+extern void on_button_released(bool was_held);
