bd48d010feca81630c18dcb4f66cb1822060b375
lilindian14 <jaime@astutesmartlocks.com>
2024-10-31 08:33:51 +1300

    Added function to update a webserver parameter with a string input

--- Firmware/AC_Link_Control/lib/CustomDRC/CDRCWebServer.hpp
+++ Firmware/AC_Link_Control/lib/CustomDRC/CDRCWebServer.hpp
@@ -3,4 +3,17 @@
 #include <stdint-gcc.h>
 
 void web_server_init(struct DSP_Settings* dsp_settings);
+
+/**
+ * void update_web_server_parameter(uint8_t parameter, uint8_t value)
+ *
+ * Update a web server parameter with the given value
+ */
 void update_web_server_parameter(uint8_t parameter, uint8_t value);
+
+/**
+ * void update_web_server_parameter_string(uint8_t parameter, char* value_string)
+ *
+ * Update a web server parameter with a string. Note, the string must be null terminated!!
+ */
+void update_web_server_parameter_string(uint8_t parameter, char* value_string);
