008083349cb53937e680b3abff57c4541b5eb778
lilindian14 <jaime@astutesmartlocks.com>
2025-01-17 11:39:33 +1300

    Update - support fully automated build version numbering system through PIO scripts

---
+++ Firmware/AC_Link_Control/include/version.h
@@ -0,0 +1,10 @@
+
+#ifndef BUILD_NUMBER
+  #define BUILD_NUMBER "15"
+#endif
+#ifndef FW_VERSION
+  #define FW_VERSION "v0.1.15 - 2025-01-17 11:37:44.658367"
+#endif
+#ifndef FW_VERSION_SHORT
+  #define FW_VERSION_SHORT "v0.1.15"
+#endif
